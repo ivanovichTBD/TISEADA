db7984763abb97482dddaac2ade9a3174ce2c25c
Erwinlero <erwinquinterosvillarroel@gmail.com>
2019-09-27 14:04:06 -0400

    interfaz

--- sistema/Registroniño.php
+++ sistema/Registroniño.php
@@ -5,9 +5,36 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="../bootstrap-4.3.1/dist/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/registroniño.css">
     <title>Document</title>
 </head>
 <body>
-    <div style="background:red">hia</div>
+<div class="container card">
+    <form role="form">
+            <div class="niño registro panel panel-default mt-2">
+                    <div class="panel-body letrasSub">
+                      REGISTRO
+                    </div>
+    </form>
+</div>
+<form role="form">
+  <div class="form-group">
+    <label for="email">Apodo:</label>
+    <input type="email" class="form-control" id="email">
+  </div>
+  <div class="form-group">
+    <label for="pwd">Edad:</label>
+    <input type="password" class="form-control" id="pwd">
+  </div>
+  <div class="form-group">
+    <label for="pwd">Contraseña:</label>
+    <input type="password" class="form-control" id="pwd">
+  </div>
+  <button type="submit" class="crecu btn btn-default">¡Crear Cuenta!</button>
+</form>
+        
+
+
 </body>
+
 </html>
