0ff0951a6ff5d4fe564ac1eb82dc95d8d13a3a8b
mario flores <react@gmail.com>
2019-10-14 15:25:48 -0400

    diseño de escribir carta

--- sistema/crear_carta.php
+++ sistema/crear_carta.php
@@ -65,43 +65,195 @@
 <html lang="es">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-	<?php include "includes/scripts.php"; ?>
+	
 	<title>Crear carta</title>
 	
 </head>
 <body>
 
-	<?php include "includes/header.php"; ?>
-	<section id="container">
+      <div align="center">
 		
-		<div class="form_register">
-			<h1>Escribe Tu Carta</h1>
-			<hr>
-			<div class="alert"><?php echo isset($alert) ? $alert : ''; ?></div>
+			    <div class="alerta"><?php echo isset($alert) ? $alert : ''; ?></div>
+                    
+                <form action="" method="POST" enctype="multipart/form-data" >
+                <h1><center>Escribe tu carta</center></h1>
 
-			<form action="" method="post" enctype="multipart/form-data">
-				<label for="nombre">Titulo de tu Carta</label>
-				<input required  type="text" name="titulo" id="titulo" placeholder="Nombre completo">
-				<label for="correo">Categoria</label>
-				<input required  type="text" name="categoria" id="categoria" placeholder="Categoria">
-                <label for="usuario">Contenido</label>
-                <textarea required  name="contenido" id="contenido"></textarea>
-				
-				<label for="nombre_imagen">Ingresa un titulo para tu foto</label>
-				<input type="text" name="nombre_imagen" id="nombre_imagen">
-				<label for="imagen">Selecciona una foto</label>
-				<input type="file" name="imagen" id="imagen">
-					
-				
-						
-				<input type="submit" value="Mandar Carta" class="btn_save">
+                <div id="from">
+                    <label for="titulo">Titulo</label>
+                    <input required type="text" id="titulo" name="titulo" placeholder="Titulo de la carta">
+                </div>
 
-			</form>
+                <div id="reply">
+                    <label for="categoria">Categoria</label>
+                    <input required type="text" id="categoria" name="categoria" placeholder="Categoria de la carta">
+                </div>
+                <div id="reply2">
+                    <label for="nombre_imagen">Titulo para tu foto</label>
+                    <input required type="text" id="nombre_imagen" name="nombre_imagen" placeholder="Nombre para tu foto">
+                </div>
+                <div id="reply3">
+                    <label for="imagen">Selecciona una foto</label>
+                    <input required type="file" id="imagen" name="imagen">
+                </div>
+                <div id="message">
+                    <label for="msg"><strong>Contenido de tu carta</strong></label>
+                    <textarea required id="msg" name="contenido"></textarea>
+                </div>
+                
+                <div class="button">
+                    <button type="submit">Envia tu carta</button>
+                </div>
+                </form>
 
-		</div>
-
-
-	</section>
-	<?php include "includes/footer.php"; ?>
+	
+      </div>
+	
 </body>
 </html>
+
+<style>
+@font-face {
+    font-family: 'handwriting';
+    src: url('img/fuentes/journal-webfont.woff2') format('woff2'),
+         url('img/fuentes/journal-webfont.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+}
+
+@font-face {
+    font-family: 'typewriter';
+    src: url('img/fuentes/veteran_typewriter-webfont.woff2') format('woff2'),
+         url('img/fuentes/veteran_typewriter-webfont.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+}
+
+body {
+  font  : 21px sans-serif;
+  padding : 2em;
+  margin  : 0;
+  background:linear-gradient(90deg, rgba(8,203,218,1) 0%, rgba(209,213,213,1) 53%, rgba(8,203,218,1) 100%);
+}
+
+form {
+  position: relative;
+  width  : 770px;
+  height : 620px;
+  margin : 0 auto;
+  border-radius:3px;
+  background: #FFF url(img/fondodecrearcarta2.jpg);
+}
+
+h1 {
+  position : absolute;
+  left : 415px;
+  top  : 45px;
+  font : 3em "typewriter", sans-serif;
+
+}
+
+#from {
+  position: absolute;
+  left : 398px;
+  top  : 245px;
+}
+
+#reply {
+  position: absolute;
+  left : 390px;
+  top  : 295px;
+}
+#reply2 {
+  position: absolute;
+  left : 380px;
+  top  : 345px;
+}
+#reply3 {
+  position: absolute;
+  left : 390px;
+  top  : 425px;
+  right: 200px;
+  
+  
+}
+
+#message {
+  position: absolute;
+  left : 20px;
+  top  : 70px;
+}
+
+
+label {
+  font : .8em "typewriter", sans-serif;
+}
+
+
+input, textarea {
+  font    : 1em "handwriting", sans-serif;
+  border  : none;
+  padding : 0 10px;
+  margin  : 0;
+  width   : 240px;
+  background: none;
+}
+
+input, textarea {
+  background   : rgba(0,0,0,.1);
+  border-radius: 5px;
+  outline      : none;
+}
+
+
+input {
+    height: 2em; /* for IE */
+    vertical-align: middle; /* This is optional but it makes legacy IEs look better */
+    font : 0.9em "typewriter", sans-serif;
+  }
+
+textarea {
+  display : block;
+  font:1.9em/1.5em "handwriting", sans-serif;
+  padding : 10px;
+  margin  : 10px 0 0 -10px;
+  width   : 340px;
+  height  : 445px;
+
+  resize  : none;
+  overflow: auto;
+}
+
+button {
+  position     : absolute;
+  left         : 440px;
+  top          : 520px;
+  padding      : 5px;
+  font : 1.3em "typewriter", sans-serif;
+  border       : 2px solid #333;
+  border-radius: 5px;
+  background   : none;
+  cursor       : pointer;
+  transform    : rotate(-1.5deg);
+  height:      40px;
+}
+
+button:after {
+  content      : " >>>";
+}
+
+button:hover,
+button:focus {
+  outline     : none;
+  background  : #001;
+  color       : #FFF;
+}
+
+.alerta{
+  background   : rgba(0,0,0,.1);
+  border-radius: 5px;
+  width  : 770px;
+  height:35px;
+  
+}
+
+</style>
