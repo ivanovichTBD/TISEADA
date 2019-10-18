8c02f4d6eaf7a4a345f1025f1cafbf239f0899a9
ivanovic3 <ivanovic.ntc@gmail.com>
2019-10-18 14:20:28 -0400

    cambios guardados

--- sistema/anadirPalabrasEnCarta.php
+++ sistema/anadirPalabrasEnCarta.php
@@ -1,28 +1,50 @@
 <?php
 if(!empty($_POST['NuevaPalabra'])){
-$PalabraNueva=$_POST['NuevaPalabra'];
-session_start();
-echo "el usuario :  ".$_SESSION['idUser']."<BR>";
-echo $PalabraNueva."<BR>";
+$PalabraNueva=strtoupper($_POST['NuevaPalabra']);
 
+session_start();
+include "../conexion.php";
+
+$usuario=$_SESSION['idUser'];
+$ConsultaUser=mysqli_query($conection,"SELECT ID_AREA FROM usuario WHERE IDUSUARIO='$usuario'");
+$resultadoConsulta=mysqli_fetch_array($ConsultaUser);
 $jsonString = file_get_contents('js/PalabrasTipo.json'); 
 $data = json_decode($jsonString, true);
+/* Distribucion de la lista de CARTAS ----------------------------------------------------------- */
+function vistaCarta($resultadoConsulta,$data){
+    if($resultadoConsulta==3){
+       $resultado = array_merge( $data[0]['Palabras'][0],$data[0]['Palabras'][1]);
+        return $resultado;
+    }elseif ($resultadoConsulta==2){
+        $resultado=$data[0]['Palabras'][2];
+        return $resultado;    
+    }else{
+        $resultado=$data[0]['Palabras'][$resultadoConsulta-1];
+        return $resultado;
+    }
+}
+
+$mostrarLista=count(vistaCarta($resultadoConsulta[0],$data));
+for ($i=0; $i < $mostrarLista ; $i++) { 
+    echo "<p>".vistaCarta($resultadoConsulta[0],$data)[$i]."</p>";
+}
 
 //array_push($data[0]['Palabras'][1],"HERMOSO");
 
 $CantPal=count($data[0]['Palabras'][1]);
-array_push($data[0]['Palabras'][1],$PalabraNueva);
+array_push($data[0]['Palabras'][$resultadoConsulta-1],$PalabraNueva);
 $newJsonString = json_encode($data); 
 
-file_put_contents('js/PalabrasTipo.json', $newJsonString);
+//file_put_contents('js/PalabrasTipo.json', $newJsonString);
 
 
 
-for ($i=0; $i <$CantPal ; $i++) { 
-   echo $data[0]['Palabras'][1][$i]."<br>";
-}
+//for ($i=0; $i <$CantPal ; $i++){ 
+ //  echo $data[0]['Palabras'][1][$i]."<br>";
+//}
 
 //var_dump($data[0]['Palabras'][1]);
+//}
 }
 ?>
 
