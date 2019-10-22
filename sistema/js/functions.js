f6b9154a739417980dba3acdf3a382ee881a2e8f
ivanovic3 <ivanovic.ntc@gmail.com>
2019-10-22 01:14:25 -0400

    palabras nuevas Interfaz

--- sistema/js/functions.js
+++ sistema/js/functions.js
@@ -1,4 +1,11 @@
-function form(){
+function ViewAnadirPalabra(){
 
-    console.log("hola que tal");
+   // var ContenidoPrincipal= document.getElementById("contenido");
+    var ContenidoLogin= document.getElementById("FormularioPalabras");
+    var estiloLogin=ContenidoLogin.getAttribute("style");
+    if(estiloLogin=="display:none"){
+        ContenidoLogin.removeAttribute('style');
+        ContenidoLogin.style.display='flow-root';
+       // ContenidoPrincipal.style.display='none';
+    }
 }
