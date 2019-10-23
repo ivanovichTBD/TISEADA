482e02e9aa21a956f0732903f2ef8bbf138b2d59
ivanovic3 <ivanovic.ntc@gmail.com>
2019-10-22 23:14:58 -0400

    vizualizacion de carta planilla 1

--- sistema/js/functions.js
+++ sistema/js/functions.js
@@ -9,3 +9,12 @@ function ViewAnadirPalabra(){
        // ContenidoPrincipal.style.display='none';
     }
 }
+function VerCarta(asunto,titulo,contenido,nombreimagen,imagen){
+    var titulo= document.getElementById("titulo").innerHTML=titulo;
+    var asunto= document.getElementById("asunto").innerHTML="ASUNTO :  "+titulo;
+    var contenido= document.getElementById("contenido").innerHTML=contenido;
+    var im= document.getElementById("nombreImagen").innerHTML=nombreimagen;
+    
+    var imag= document.getElementById("imagen");
+    var imagen=imag.style.backgroundImage = "url('"+imagen+"')";
+}
